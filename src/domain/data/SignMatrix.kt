142d03bb2d62ab755b385f32c65357ad8664144e
Dima Barannik <dimas97@mail.ru>
2019-03-16 11:36:04 +0200

    create matrix

--- src/domain/data/SignMatrix.kt
+++ src/domain/data/SignMatrix.kt
@@ -1,14 +1,6 @@
 package domain.data
 
-class SignMatrix(private val data: List<List<Boolean>>) {
-    constructor(width: Int, height: Int, init: (Int, Int) -> Boolean) :
-            this(List(width) { i -> List(height) { j -> init(i, j) } })
+import domain.data.generic.Matrix
 
-    val width: Int
-        get() = data.size
-
-    val height: Int
-        get() = data.first().size
-
-    fun signAt(i: Int, j: Int) = data[i][j]
-}
+class SignMatrix(width: Int, height: Int, init: (Int, Int) -> Boolean) :
+    Matrix<Boolean>(width, height, init)
