ea7fa16895b33ba46abd70d8154a4cd9ccf39ae9
Dima Barannik <dimas97@mail.ru>
2019-03-16 12:06:03 +0200

    create ByteVector.kt

---
+++ src/domain/data/subtype/ByteVector.kt
@@ -0,0 +1,8 @@
+package domain.data.subtype
+
+class ByteVector : Iterable<Byte> {
+    override fun iterator(): Iterator<Byte> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
