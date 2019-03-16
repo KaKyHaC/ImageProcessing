be8eda131ed744b3880ef510e7cd1c477be1f346
Dima Barannik <dimas97@mail.ru>
2019-03-16 12:29:47 +0200

    MatrixContainer.kt: improve

--- src/domain/data/generic/MatrixContainer.kt
+++ src/domain/data/generic/MatrixContainer.kt
@@ -1,3 +1,8 @@
 package domain.data.generic
 
-open class MatrixContainer<T> (val data: List<T>)
+open class MatrixContainer<T> (private val data: MutableList<T>) {
+    val dimension: Int
+        get() = data.size
+
+    operator fun get(i: Int) = data[i]
+}
