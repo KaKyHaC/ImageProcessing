ac60317c031ff117791cdee887aded223dab8168
Dima Barannik <dimas97@mail.ru>
2019-03-16 11:37:43 +0200

    improve matrix

--- src/domain/data/generic/Matrix.kt
+++ src/domain/data/generic/Matrix.kt
@@ -1,6 +1,6 @@
 package domain.data.generic
 
-open class Matrix<T> (private val data: List<MutableList<T>>) {
+open class Matrix<T>(private val data: List<MutableList<T>>) {
     constructor(width: Int, height: Int, init: (Int, Int) -> T) :
             this(List(width) { i -> MutableList(height) { j -> init(i, j) } })
 
@@ -11,8 +11,13 @@ open class Matrix<T> (private val data: List<MutableList<T>>) {
         get() = data.first().size
 
     fun getAt(i: Int, j: Int) = data[i][j]
-    fun setAt(i: Int, j: Int, value: T) { data[i][j] = value}
+    fun setAt(i: Int, j: Int, value: T) {
+        data[i][j] = value
+    }
 
-    operator fun set(i: Int, j: Int, value: T) { data[i][j] = value}
+    operator fun set(i: Int, j: Int, value: T) {
+        data[i][j] = value
+    }
+
     operator fun get(i: Int, j: Int) = data[i][j]
 }
