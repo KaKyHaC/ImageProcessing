8656b83997c64ff12f48694d8c21d54fa3a78c39
Dima Barannik <dimas97@mail.ru>
2019-03-16 11:18:02 +0200

    ProcessedDataUnit.kt: improve

--- src/domain/data/ProcessedDataUnit.kt
+++ src/domain/data/ProcessedDataUnit.kt
@@ -2,4 +2,18 @@ package domain.data
 
 import java.math.BigInteger
 
-data class ProcessedDataUnit(val code: BigInteger)
+data class ProcessedDataUnit(
+    val code: BigInteger,
+    val baseArray: ShortArray,
+    val signMatrix: SignMatrix,
+    val width: Int,
+    val height: Int
+) {
+    init {
+        if (signMatrix.width != width || signMatrix.height != height)
+            throw Exception("invalid sign matrix size")
+
+        if (baseArray.size != height)
+            throw Exception("invalid base array size")
+    }
+}
