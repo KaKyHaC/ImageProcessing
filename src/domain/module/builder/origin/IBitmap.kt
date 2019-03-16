06223a12a636bffe0a6c5a548f964a6a409da8b6
Dima Barannik <dimas97@mail.ru>
2019-03-16 12:38:29 +0200

    OriginBuilder.kt: improve

--- src/domain/module/builder/origin/IBitmap.kt
+++ src/domain/module/builder/origin/IBitmap.kt
@@ -3,6 +3,10 @@ package domain.module.builder.origin
 interface IBitmap {
     val width: Int
     val height: Int
-    fun getRgb(i: Int, j:Int): Int
+    fun getRgb(i: Int, j: Int): Int
     fun setRgb(i: Int, j:Int, value: Int)
+
+    interface Builder{
+        fun build(width: Int, height: Int, init: (Int, Int) -> Int): IBitmap
+    }
 }
--- src/domain/module/builder/origin/OriginBuilder.kt
+++ src/domain/module/builder/origin/OriginBuilder.kt
@@ -5,17 +5,22 @@ import domain.data.origin.OriginDataMatrix
 import domain.data.origin.OriginDataUnit
 import domain.module.IModule
 
-class OriginBuilder : IModule<IBitmap, OriginDataContainer> {
+class OriginBuilder(val builder: IBitmap.Builder) : IModule<IBitmap, OriginDataContainer> {
     override fun direct(data: IBitmap): OriginDataContainer {
         val matrixList = List(3) { dimension ->
             OriginDataMatrix(data.width, data.height) { i, j ->
-                OriginDataUnit((data.getRgb(i, j) shr dimension * 8 and 0xff).toShort())
+                OriginDataUnit((data.getRgb(i, j) shr dimension * 8 and 0xff))
             }
         }
         return OriginDataContainer(matrixList)
     }
 
     override fun reverse(data: OriginDataContainer): IBitmap {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return builder.build(data[0].width, data[0].height) { i, j ->
+            val blue = data[0][i, j].toInt()
+            val green = data[1][i, j].toInt()
+            val red = data[2][i, j].toInt()
+            blue or (green shl 8) or (red shl 16)
+        }
     }
 }
