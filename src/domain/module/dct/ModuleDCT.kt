36d587141022fa5d0ec5b75d77d6fb24ce915641
Dima Barannik <dimas97@mail.ru>
2019-03-17 11:28:27 +0200

    ModuleDCT.kt: clear

--- src/domain/module/dct/ModuleDCT.kt
+++ src/domain/module/dct/ModuleDCT.kt
@@ -1,15 +1,5 @@
 package domain.module.dct
 
-import domain.data.IData
-import domain.module.IModule
+class ModuleDCT {
 
-class ModuleDCT : IModule {
-
-    override fun direct(data: IData): IData {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun reverse(data: IData): IData {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 }
