2171033a0d0a9bd674cdb1c971a81c4839e84348
Dima Barannik <dimas97@mail.ru>
2019-03-17 11:52:02 +0200

    ModuleNode.kt: use lambda

--- src/domain/ModuleNode.kt
+++ src/domain/ModuleNode.kt
@@ -1,9 +1,8 @@
 package domain
 
-import domain.data.IData
 import domain.module.IModule
 
-class ModuleNode<InputType : IData, OutputType : IData>(val module: IModule<InputType, OutputType>) {
+class ModuleNode<InputType, OutputType>(val module: IModule<InputType, OutputType>) {
     var invokeNext: ((OutputType) -> Unit)? = null
 
     fun direct(data: InputType) {
