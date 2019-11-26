e00187510e82df414a6bb55cb21ea4e13e5aedbc
Nikita <nutsdev777@gmail.com>
2019-11-26 16:55:31 +0200

    ByteVector.kt: update TODO

--- ImageProcessingLib/src/data/types/ByteVector.kt
+++ ImageProcessingLib/src/data/types/ByteVector.kt
@@ -3,7 +3,6 @@ package data.types
 import constants.Constants
 import data.interfaces.IByteVector
 import java.nio.ByteBuffer
-import kotlin.experimental.or
 
 /**
  * Non save class
@@ -24,61 +23,18 @@ class ByteVector : IByteVector {
     override val bytes: ByteArray
         get() = data.toByteArray()
 
+    override fun append(value: Long, bitLength: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getBits(bitLength: Int, position: Int?): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun iterator() = data.iterator()
+
     fun resetIterator() {
         bitIndex = 0
         byteIndex = 0
     }
-
-    override fun append(value: Boolean) {
-        val byteValue = if (value) Math.pow(2.0, byteIndex.toDouble()).toByte() else 0
-        val byteIndex = if (bitIndex == 0) byteIndex++ else byteIndex - 1
-        val currentByte = data.getOrNull(byteIndex) ?: 0
-        val newByte = currentByte or byteValue
-        data.set(byteIndex, newByte)
-    }
-
-    override fun append(value: Byte) {
-        data.add(value)
-    }
-
-    override fun append(value: Short) {
-        bitIndex = 0
-        data.addAll(byteBuffer.putShort(value).array().toList())
-    }
-
-    override fun append(value: Int) {
-        bitIndex = 0
-        data.addAll(byteBuffer.putInt(value).array().toList())
-    }
-
-    override fun append(value: Long) {
-        bitIndex = 0
-        data.addAll(byteBuffer.putLong(value).array().toList())
-    }
-
-    override fun getNextBoolean(index: Int?): Boolean? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getNextByte(index: Int?) = data.getOrNull(index ?: byteIndex++)
-
-    override fun getNextShort(index: Int?): Short? {
-        byteBuffer.put(bytes, index ?: byteIndex++, 2)
-        byteBuffer.flip()//need flip
-        return byteBuffer.short
-    }
-
-    override fun getNextInt(index: Int?): Int? {
-        byteBuffer.put(bytes, index ?: byteIndex++, 4)
-        byteBuffer.flip()//need flip
-        return byteBuffer.int
-    }
-
-    override fun getNextLong(index: Int?): Long? {
-        byteBuffer.put(bytes, index ?: byteIndex++, 8)
-        byteBuffer.flip()//need flip
-        return byteBuffer.long
-    }
-
-    override fun iterator() = data.iterator()
 }
