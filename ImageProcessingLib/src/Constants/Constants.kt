0425e8e0ed8b5cc72c3bac8a9dbc8a5bb87b9407
Nikita <nutsdev777@gmail.com>
2019-11-26 16:31:25 +0200

    create Constants: package

---
+++ ImageProcessingLib/src/Constants/Constants.kt
@@ -0,0 +1,16 @@
+package ImageCompressionLib.Constants
+
+import java.math.BigInteger
+
+object Constants {
+    const val SIZE_OF_BLOCK = 8;
+    const val BITS_PER_BYTE = 8
+    const val BYTES_PER_LONG = 8
+    const val ONE_LEFT_BIT = 0b10000000
+    val BI_TWO = BigInteger.valueOf(2)
+    var MAX_LONG = Math.pow(2.0, 54.0).toLong()
+    var BINARY_VALUE_OF_POSITION = 1
+    var DEFAULT_PASSWORD = " "
+    val MAX_QUANTIZATION_VALUE = 55
+    val DEFAULT_MATRIX_VALUE = 128.toShort()
+}
---
+++ ImageProcessingLib/src/Constants/QuantizationTable.kt
@@ -0,0 +1,53 @@
+package ImageCompressionLib.Constants
+
+/**
+ * Created by Димка on 08.08.2016.
+ */
+object QuantizationTable {
+
+    val luminosity = arrayOf(
+        shortArrayOf(1, 2, 3, 4, 5, 6, 7, 8),
+        shortArrayOf(2, 2, 3, 4, 5, 8, 10, 15),
+        shortArrayOf(3, 4, 5, 6, 7, 9, 16, 20),
+        shortArrayOf(4, 6, 8, 19, 21, 27, 28, 36),
+        shortArrayOf(5, 12, 27, 36, 48, 59, 69, 77),
+        shortArrayOf(7, 35, 35, 44, 51, 69, 79, 82),
+        shortArrayOf(9, 44, 47, 48, 49, 59, 69, 79),
+        shortArrayOf(12, 49, 55, 68, 79, 80, 99, 99)
+    )
+
+    val chromaticity = arrayOf(
+        shortArrayOf(1, 1, 2, 4, 9, 99, 99, 99),
+        shortArrayOf(1, 2, 2, 6, 99, 99, 99, 99),
+        shortArrayOf(2, 2, 56, 99, 99, 99, 99, 99),
+        shortArrayOf(4, 66, 99, 99, 99, 99, 99, 99),
+        shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99),
+        shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99),
+        shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99),
+        shortArrayOf(99, 99, 99, 99, 99, 99, 99, 99)
+    )
+
+
+    private var smart: Array<ShortArray>? = null
+    private var smartValue: Int = 0
+
+    private fun initSmart(value: Int) {
+        smartValue = value
+        smart = Array(Constants.SIZE_OF_BLOCK) { i ->
+            ShortArray(Constants.SIZE_OF_BLOCK) { j ->
+                (1 + value * (i + j)).toShort()
+            }
+        }
+    }
+
+
+    fun getLuminosity(i: Int, j: Int) = luminosity[i][j]
+
+    fun getChromaticity(i: Int, j: Int) = chromaticity[i][j]
+
+    fun getSmart(value: Int, i: Int, j: Int): Short {
+        if (smart == null || value != smartValue)
+            initSmart(value)
+        return smart!![i][j]
+    }
+}
